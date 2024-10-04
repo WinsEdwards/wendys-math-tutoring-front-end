d84b3e38347a08e198ea2322de2cdd31ab3f16bf
Winslow <owedwards@gmail.com>
2024-10-04 15:47:57 -0400

    Created initial components and associated files, imported components to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from '.components/Navbar';
+import About from '.components/About';
+import Testimonials from '.components/Testimonials';
+import Approach from '.components/Approach';
+import Topics from '.components/Topics';
+import ServiceArea from '.components/ServiceArea';
+import Samples from '.components/Samples';
+import Scheduling from '.components/Scheduling';
+import BizInfo from '.components/BizInfo';
+import Contact from '.components/Contact';
+import Footer from '.components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <About />
+      <Testimonials />
+      <Approach />
+      <Topics />
+      <ServiceArea />
+      <Samples />
+      <Scheduling />
+      <BizInfo />
+      <Contact />
+      <Footer />
+    </main>
   );
 }
 
